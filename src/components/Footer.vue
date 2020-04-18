312b396a8841c65f351821df0d6e400aa3689de4
karolinabigaj <karolinabigaj@gmail.com>
2020-04-18 20:06:17 +0200

    Favicon

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,8 @@
     <footer>
         <span>
             Icons made by
-            <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>
+            <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> and
+            <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a>
             from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
         </span>
     </footer>
