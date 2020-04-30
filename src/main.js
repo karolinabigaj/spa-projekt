a70139b0f087ada2b6bc4d44f3f91e15acddd8d8
Leczo <maciek.czarn@gmail.com>
2020-04-30 04:47:13 +0200

    add Converter

--- src/main.js
+++ src/main.js
@@ -4,9 +4,12 @@ import BootstrapVue from 'bootstrap-vue'
 import VueRouter from 'vue-router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import "vue-chartjs"; // npm install vue-chartjs chart.js --save
+import "vue-chartjs";             // npm install vue-chartjs chart.js --save
 import Converter from './components/Converter.vue';
 import Chart from './components/Chart.vue';
+import axios from 'axios'         //npm install --save vue-axios
+import VueAxios from 'vue-axios'  // npm install axios --save
+Vue.use(VueAxios, axios)
 
 Vue.use(BootstrapVue)
 Vue.use(VueRouter)
