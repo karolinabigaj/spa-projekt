0b343ba71b41b1c0cc770b387117e88ec8b3a407
Maciej Faron <maciejfaron@o2.pl>
2020-04-30 04:20:55 +0200

    add charts

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,53 +1,13 @@
-<template>
-  <div class="small">
-    <line-chart :chart-data="datacollection"></line-chart>
-    <button @click="fillData()">Randomize</button>
-  </div>
-</template>
-
 <script>
-import LineChart from "./LineChart.js";
+import { Line, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
 
 export default {
-  components: {
-    LineChart
-  },
-  data() {
-    return {
-      datacollection: null
-    };
-  },
-  mounted() {
-    this.fillData();
-  },
-  methods: {
-    fillData() {
-      this.datacollection = {
-        labels: [this.getRandomInt(), this.getRandomInt()],
-        datasets: [
-          {
-            label: "Data One",
-            backgroundColor: "#f87979",
-            data: [this.getRandomInt(), this.getRandomInt()]
-          },
-          {
-            label: "Data One",
-            backgroundColor: "#f87979",
-            data: [this.getRandomInt(), this.getRandomInt()]
-          }
-        ]
-      };
-    },
-    getRandomInt() {
-      return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+    extends: Line,
+    mixins: [reactiveProp],
+    props: ['options'],
+    mounted() {
+        this.renderChart(this.chartData, this.options)
     }
-  }
-};
-</script>
-
-<style>
-.small {
-  max-width: 600px;
-  margin: 150px auto;
 }
-</style>
+</script>
